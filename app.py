3e508aff06c1ba045867060433c2208cc8a390d7
CarlosArce-tes <arcecarlos1c@gmail.com>
2023-11-09 21:03:36 -0600

    Mapa con vista de satelite

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from itertools import permutations
 import folium
 from flask import Flask, render_template
 
+
+from folium import Map, LayerControl
 app = Flask(__name__)
 
 # Coordenadas de las ubicaciones
@@ -12,8 +14,10 @@ locations = [
     ("Puebla", 19.0402, -98.2062),
     ("Cuernavaca", 18.9242, -99.2216),
     ("Querétaro", 20.5888, -100.3899),
-]
+    ("Guadalajara", 20.6597, -103.3496),
+    ("Monterrey", 25.6866, -100.3161),
 
+]
 # Función para calcular la distancia total de una secuencia de entregas
 def calcular_distancia_total(secuencia):
     distancia_total = 0
@@ -33,15 +37,44 @@ for secuencia in permutaciones_posibles:
     if distancia_actual < mejor_distancia:
         mejor_distancia = distancia_actual
         mejor_secuencia = secuencia
-
+'''
 # Crea un mapa y agrega marcadores para las ubicaciones
-m = folium.Map(location=locations[mejor_secuencia[0]][1:], zoom_start=8)
 
+m = folium.Map(location=locations[mejor_secuencia[0]][1:], zoom_start=8)
+LayerControl().add_to(m)
 for i, ubicacion in enumerate(mejor_secuencia):
     folium.Marker(
         location=locations[ubicacion][1:],
         popup=locations[ubicacion][0],
     ).add_to(m)
+folium.TileLayer(
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Esri World Imagery',
+    name='Satélite',
+).add_to(m)
+'''
+
+
+m = folium.Map(location=locations[0][1:], zoom_start=8)
+
+# Agregar marcadores para las ubicaciones
+for location in locations:
+    folium.Marker(
+        location=location[1:],
+        popup=location[0],
+    ).add_to(m)
+
+# Agregar una capa de relieve (Stamen Terrain)
+folium.TileLayer(
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Esri World Imagery',
+    name='Satélite',
+).add_to(m)
+
+
+
+
+
 
 # Crea una línea para mostrar la ruta óptima
 ruta_optima = [locations[i][1:] for i in mejor_secuencia]
@@ -49,6 +82,7 @@ folium.PolyLine(ruta_optima, color="blue", weight=2.5, opacity=1).add_to(m)
 
 # Guarda el mapa en un archivo HTML
 m.save("./templates/ruta_optima_map.html")
+
 @app.route('/ruta_optima_map')
 def ruta_optima_map():
     return render_template('ruta_optima_map.html', mejor_secuencia=mejor_secuencia)
