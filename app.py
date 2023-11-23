125879237002857a6eb21a02cc10347cad62fb7e
CarlosArce-tes <arcecarlos1c@gmail.com>
2023-11-23 11:15:58 -0600

    Eliminacion de rutas con boton

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import random
 from geopy.distance import great_circle
 from itertools import permutations
 import folium
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, redirect, url_for
 import mysql.connector
-
 
 app = Flask(__name__)
 
-# Función para conectar con la base de datos y obtener las coordenadas
 def obtener_coordenadas():
-    # Conecta con la base de datos
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
@@ -19,28 +16,22 @@ def obtener_coordenadas():
     )
     cursor = conn.cursor()
 
-    # Ejecuta una consulta para obtener las coordenadas
     cursor.execute("SELECT IDCoordenada, NombreLugar, Latitud, Longitud FROM Coordenadas")
     coordenadas_db = cursor.fetchall()
 
-    # Convierte las coordenadas a un formato compatible
-    locations = [(coordenada[1], float(coordenada[2]), float(coordenada[3])) for coordenada in coordenadas_db]
+    # Correct the variable name to 'coordenada' in the list comprehension
+    locations = [(coordenada[0], coordenada[1], float(coordenada[2]), float(coordenada[3])) for coordenada in coordenadas_db]
 
-    # Cierra la conexión con la base de datos
     cursor.close()
     conn.close()
 
     return locations
 
-# Función para calcular la mejor secuencia de entregas
 def calcular_mejor_secuencia(locations):
-    # Genera todas las permutaciones posibles de las ubicaciones
     permutaciones_posibles = permutations(range(len(locations)))
-
     mejor_secuencia = None
     mejor_distancia = float('inf')
 
-    # Encuentra la secuencia de entregas con la distancia total más corta
     for secuencia in permutaciones_posibles:
         distancia_actual = calcular_distancia_total(locations, secuencia)
         if distancia_actual < mejor_distancia:
@@ -49,12 +40,10 @@ def calcular_mejor_secuencia(locations):
 
     return mejor_secuencia
 
-# Función para calcular la distancia total de una secuencia de entregas
-
 def calcular_distancia_total(locations, secuencia):
     distancia_total = 0
     for i in range(len(secuencia) - 1):
-        distancia_total += great_circle(locations[secuencia[i]][1:], locations[secuencia[i + 1]][1:]).meters
+        distancia_total += great_circle(locations[secuencia[i]][2:], locations[secuencia[i + 1]][2:]).meters
 
     return distancia_total
 
@@ -62,49 +51,64 @@ def calcular_distancia_total(locations, secuencia):
 def ruta_optima_map():
     locations = obtener_coordenadas()
     mejor_secuencia = calcular_mejor_secuencia(locations)
+    
+    m = folium.Map(location=locations[mejor_secuencia[0]][2:], zoom_start=8)
 
-    # Crea un mapa y agrega marcadores para las ubicaciones
-    m = folium.Map(location=locations[mejor_secuencia[0]][1:], zoom_start=8)
-
-    # Agrega una capa de satélite de OpenStreetMap
     folium.TileLayer(
         tiles='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         attr='OpenStreetMap',
         name='Mapa',
     ).add_to(m)
 
-    # Agrega una capa de satélite de OpenStreetMap (Esri World Imagery)
     folium.TileLayer(
         tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
         attr='Esri World Imagery',
         name='Satélite',
     ).add_to(m)
 
-    # Agrega control de capas para cambiar entre la vista de mapa y satélite
     folium.LayerControl().add_to(m)
 
-    # Agrega marcadores y línea de la ruta
     for i, ubicacion in enumerate(mejor_secuencia):
         folium.Marker(
-            location=locations[ubicacion][1:],
-            popup=locations[ubicacion][0]
+            location=locations[ubicacion][2:],
+            popup=f'{locations[ubicacion][0]} \n {locations[ubicacion][1]}'
         ).add_to(m)
 
-    ruta_optima = [locations[i][1:] for i in mejor_secuencia]
+    ruta_optima = [locations[i][2:] for i in mejor_secuencia]
     folium.PolyLine(ruta_optima, color="blue", weight=2.5, opacity=1).add_to(m)
 
-    # Guarda el mapa en un archivo HTML
     m.save("./templates/ruta_optima_map.html")
 
     return render_template('ruta_optima_map.html', mejor_secuencia=mejor_secuencia)
 
 @app.route('/')
 def index():
+    nombre = 'carlos'
     locations = obtener_coordenadas()
     mejor_secuencia = calcular_mejor_secuencia(locations)
 
-    return render_template('index.html', ruta_optima=mejor_secuencia, locations=locations)
+    return render_template('index.html', ruta_optima=mejor_secuencia, locations=locations, nombre=nombre)
 
+@app.route('/eliminar_ubicacion/<int:id>')
+def eliminar_ubicacion(id):
+    print(f"Received ID: {id}")
+
+    conn = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='carlos18',
+        database='seguiorden'
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM Coordenadas WHERE IDCoordenada = %s", (id,))
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
